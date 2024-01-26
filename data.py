36c072854ccea34bf8cda60a8780712088c72a6c
Marco Soares <106749775+Sengeki1@users.noreply.github.com>
2024-01-26 16:48:47 -0100

    Splitting the data into train and validation set

--- data.py
+++ data.py
@@ -1,8 +1,11 @@
+import numpy as np
 import os
 import pickle
+import matplotlib.pyplot as plt
 
 def load_Data(path):
     files = os.listdir(path=path)
+    
     train = []
     trainlabel = []
     test = []
@@ -17,10 +20,35 @@ def load_Data(path):
             else:
                 test.append(dict[b'data'])
                 testlabel.append(dict[b'labels'])
-
     
-    dictData = {}
-    
+    print(train)
 
+    dictData = {'train': np.array(train), 'trainlabel': np.array(trainlabel), 
+                'test': np.array(test), 'testlabel': np.array(testlabel)}
+    
+    # We reshape the array into a new array where we gonna store it in a dictionary as a value on this specific key
+    for key in dictData:
+        if key == 'train' or key == 'test':
+            dictData[key] = np.reshape(dictData[key], newshape=(dictData[key].shape[0] * dictData[key].shape[1], dictData[key].shape[2]))
+        else:
+            dictData[key] = np.reshape(dictData[key], newshape=(dictData[key].shape[0] * dictData[key].shape[1]))
+    return dictData
 
 data = load_Data('cifar-10-batches-py')
+
+# visualizing 
+temp = data['test'][45] # taking a sample from the testing data
+
+R = temp[0:1024].reshape(32, 32)
+G = np.reshape(temp[1024:2048], newshape=(32, 32))
+B = np.reshape(temp[2048:], newshape=(32, 32))
+temp = np.dstack((R, G, B)) # stacking them together as a sequence depth wise (along third axis) 
+plt.imshow(temp)
+plt.show()
+
+# Splitting the data into train and validation set
+x_train, y_train = data['train'], data['trainlabel']
+x_test, y_test = data['test'], data['testlabel']
+
+train_x, val_x = np.split(x_train, [int(0.98*len(x_train))])
+train_y, val_y = np.split(y_train, [int(0.98*len(y_train))])
